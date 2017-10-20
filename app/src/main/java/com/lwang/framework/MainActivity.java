585bea33aa26f41ed7809e124ff8bc516e49313d
wanglu <lwang057@163.com>
2017-10-20 15:42:54 +0800

    first commit

--- app/src/main/java/com/lwang/framework/MainActivity.java
+++ app/src/main/java/com/lwang/framework/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lwang.framework;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+    public void button(View view) {
+
+        Toast.makeText(this, "HelloWorld", Toast.LENGTH_SHORT).show();
+    }
+
 }
